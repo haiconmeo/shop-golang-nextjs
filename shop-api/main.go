69a60c2d7250e8b2f0c991a9d5675e848122ff9c
haiconmeo <na187729288@gmail.com>
2023-04-17 20:57:10 +0700

    shop

---
+++ shop-api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type ProductMedia struct {
+	gorm.Model
+	Image     string `json:"image" gorm:"image"`
+	Position  int    `json:"position" gorm:"position"`
+	ProductId int    `json:"productId" gorm:"product_id"`
+}
+type Product struct {
+	gorm.Model
+	Name        string `json:"name" gorm:"name"`
+	Description string `json:"description" gorm:"description"`
+	Price       uint   `json:"price" gorm:"price"`
+	Media       []ProductMedia
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("err db")
+	}
+	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&ProductMedia{})
+	
+}
